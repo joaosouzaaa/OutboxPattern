b6724f1c9858cac6448ea633202158a19c626b68
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-25 22:40:36 -0300

    Tests: TicketValidator UnitTests.

---
+++ Tickets.UnitTests/TestBuilders/TicketBuilder.cs
@@ -0,0 +1,56 @@
+﻿using Tickets.Microservice.Entities;
+
+namespace Tickets.UnitTests.TestBuilders;
+public sealed class TicketBuilder
+{
+    private readonly Guid _id = Guid.NewGuid();
+    private string _title = "test";
+    private int _number = 123;
+    private string _tag = "rand";
+    private string _description = "random with more than 20 characters";
+    private readonly DateTime _createdDate = DateTime.Now;
+    private readonly DateTime _firstAppearance = DateTime.Now;
+
+    public static TicketBuilder NewObject() =>
+        new();
+
+    public Ticket DomainBuild() =>
+        new()
+        {
+            FirstAppearance = _firstAppearance,
+            CreatedDate = _createdDate,
+            Description = _description,
+            Id = _id,
+            Number = _number,
+            Tag = _tag,
+            Title = _title
+        };
+
+    public TicketBuilder WithTitle(string title)
+    {
+        _title = title;
+
+        return this;
+    }
+
+    public TicketBuilder WithNumber(int number)
+    {
+        _number = number;
+
+        return this;
+    }
+
+    public TicketBuilder WithTag(string tag)
+    {
+        _tag = tag;
+
+        return this;
+    }
+
+    public TicketBuilder WithDescription(string description)
+    {
+        _description = description;
+
+        return this;
+    }
+}
---
+++ Tickets.UnitTests/ValidatorsTests/TicketValidatorTests.cs
@@ -0,0 +1,114 @@
+﻿using Tickets.Microservice.Validators;
+using Tickets.UnitTests.TestBuilders;
+
+namespace Tickets.UnitTests.ValidatorsTests;
+public sealed class TicketValidatorTests
+{
+    private readonly TicketValidator _ticketValidator;
+
+    public TicketValidatorTests()
+    {
+        _ticketValidator = new TicketValidator();
+    }
+
+    [Fact]
+    public async Task ValidateAsync_SuccessfulScenario_ReturnsTrue()
+    {
+        // A
+        var ticketToValidate = TicketBuilder.NewObject().DomainBuild();
+
+        // A
+        var validationResult = await _ticketValidator.ValidateAsync(ticketToValidate);
+
+        // A
+        Assert.True(validationResult.IsValid);
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidTitleParameters))]
+    public async Task ValidateAsync_InvalidTitle_ReturnsFalse(string title)
+    {
+        // A
+        var ticketWithInvalidTitle = TicketBuilder.NewObject().WithTitle(title).DomainBuild();
+
+        // A
+        var validationResult = await _ticketValidator.ValidateAsync(ticketWithInvalidTitle);
+
+        // A
+        Assert.False(validationResult.IsValid);
+    }
+
+    public static TheoryData<string> InvalidTitleParameters() =>
+        new()
+        {
+            "",
+            new string('a', 151),
+            "aa"
+        };
+
+    [Theory]
+    [MemberData(nameof(InvalidNumberParameters))]
+    public async Task ValidateAsync_InvalidNumber_ReturnsFalse(int number)
+    {
+        // A
+        var ticketWithInvalidNumber = TicketBuilder.NewObject().WithNumber(number).DomainBuild();
+
+        // A
+        var validationResult = await _ticketValidator.ValidateAsync(ticketWithInvalidNumber);
+
+        // A
+        Assert.False(validationResult.IsValid);
+    }
+
+    public static TheoryData<int> InvalidNumberParameters() =>
+        new()
+        {
+            0,
+            -5
+        };
+
+    [Theory]
+    [MemberData(nameof(InvalidTagParameters))]
+    public async Task ValidateAsync_InvalidTag_ReturnsFalse(string tag)
+    {
+        // A
+        var ticketWithInvalidTag = TicketBuilder.NewObject().WithTag(tag).DomainBuild();
+
+        // A
+        var validationResult = await _ticketValidator.ValidateAsync(ticketWithInvalidTag);
+
+        // A
+        Assert.False(validationResult.IsValid);
+    }
+
+    public static TheoryData<string> InvalidTagParameters() =>
+        new()
+        {
+            "",
+            new string('a', 151)
+        };
+
+    [Theory]
+    [MemberData(nameof(InvalidDescriptionParameters))]
+    public async Task ValidateAsync_InvalidDescription_ReturnsFalse(string description)
+    {
+        // A
+        var ticketWithInvalidDescription = TicketBuilder.NewObject().WithDescription(description).DomainBuild();
+
+        // A
+        var validationResult = await _ticketValidator.ValidateAsync(ticketWithInvalidDescription);
+
+        // A
+        Assert.False(validationResult.IsValid);
+    }
+
+    public static TheoryData<string> InvalidDescriptionParameters() =>
+        new()
+        {
+            "",
+            new string('a', 2001),
+            "aaaa",
+            "test",
+            "random"
+        };
+}
