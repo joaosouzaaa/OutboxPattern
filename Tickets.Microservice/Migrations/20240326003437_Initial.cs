b4f05f7b85d28938883dbcb269f96955728ffab6
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-25 21:35:02 -0300

    Feat: Adding initial migration.

---
+++ Tickets.Microservice/Migrations/20240326003437_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tickets.Microservice.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ticket",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    title = table.Column<string>(type: "varchar(150)", nullable: false),
+                    number = table.Column<int>(type: "int", nullable: false),
+                    tag = table.Column<string>(type: "varchar(150)", nullable: false),
+                    description = table.Column<string>(type: "varchar(2000)", nullable: false),
+                    created_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    first_appearance = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ticket", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ticket");
+        }
+    }
+}
