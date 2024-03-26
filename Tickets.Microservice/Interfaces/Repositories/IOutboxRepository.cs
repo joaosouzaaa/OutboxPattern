b44783df63d03bea2744b3ab31534f518240b1ce
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-25 22:11:39 -0300

    Feat: Adding OutboxRepository implementation.

---
+++ Tickets.Microservice/Interfaces/Repositories/IOutboxRepository.cs
@@ -0,0 +1,10 @@
+﻿using Tickets.Microservice.Entities;
+
+namespace Tickets.Microservice.Interfaces.Repositories;
+
+public interface IOutboxRepository
+{
+    Task<bool> AddAsync(Outbox outbox);
+    Task<bool> UpdateProcessedDateAsync(Guid id, DateTime processedDate);
+    Task<List<Outbox>> GetAllUnprocessedMessagesAsync();
+}
