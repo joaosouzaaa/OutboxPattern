17667453813ecd8a70923f7ab3b6c067a1971c57
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-25 21:22:01 -0300

    Feat: Adding pagination and connection string.

--- Tickets.Microservice/Entities/Ticket.cs
+++ Tickets.Microservice/Entities/Ticket.cs
@@ -4,6 +4,8 @@ public sealed class Ticket
 {
     public Guid Id { get; set; }
     public required string Title { get; set; }
+    public required int Number { get; set; }
+    public required string Tag { get; set; }
     public required string Description { get; set; }
     public required DateTime CreatedDate { get; set; }
     public required DateTime FirstAppearance { get; set; }
