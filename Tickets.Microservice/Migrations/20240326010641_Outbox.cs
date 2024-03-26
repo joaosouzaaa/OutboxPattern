32faa1373080335660436e51d0cf89032a3f3e63
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-25 22:06:53 -0300

    Feat: Adding OutboxMapping and migration.

---
+++ Tickets.Microservice/Migrations/20240326010641_Outbox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tickets.Microservice.Migrations
+{
+    /// <inheritdoc />
+    public partial class Outbox : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Outbox",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    created_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    content = table.Column<string>(type: "varchar(MAX)", nullable: false),
+                    type = table.Column<string>(type: "varchar(100)", nullable: false),
+                    processed_date = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Outbox", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Outbox");
+        }
+    }
+}
