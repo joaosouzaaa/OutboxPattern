9c49a67923d87eaf9ea70d27079b6e077bd51f7e
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-25 21:15:28 -0300

    Feat: Adding initial entity.

---
+++ Tickets.Microservice/Entities/Ticket.cs
@@ -0,0 +1,10 @@
+﻿namespace Tickets.Microservice.Entities;
+
+public sealed class Ticket
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required DateTime CreatedDate { get; set; }
+    public required DateTime FirstAppearance { get; set; }
+}
