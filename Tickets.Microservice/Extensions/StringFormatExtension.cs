6604729cb7db909ea70a0a457378caffedd09b11
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-25 21:06:46 -0300

    Feat: Adding message for errors and extensions.

---
+++ Tickets.Microservice/Extensions/StringFormatExtension.cs
@@ -0,0 +1,7 @@
+﻿namespace Tickets.Microservice.Extensions;
+
+public static class StringFormatExtension
+{
+    public static string FormatTo(this string message, params object[] args) =>
+       string.Format(message, args);
+}
