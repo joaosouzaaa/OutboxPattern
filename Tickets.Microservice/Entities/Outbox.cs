b881a23b184c559976d2a0223ba0f4b284e2fb2f
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-25 22:01:57 -0300

    Feat: Adding Outbox class.

---
+++ Tickets.Microservice/Entities/Outbox.cs
@@ -0,0 +1,10 @@
+﻿namespace Tickets.Microservice.Entities;
+
+public sealed class Outbox
+{
+    public Guid Id { get; set; }
+    public required DateTime CreatedDate { get; set; }
+    public required string Content { get; set; }
+    public required string Type { get; set; }
+    public DateTime? ProcessedDate { get; set; }
+}
