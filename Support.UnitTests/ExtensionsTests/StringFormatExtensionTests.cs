78fadc9d77b2b6596f48d96903ecb949b68be783
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-26 21:38:24 -0300

    Tests: Extensions UnitTests.

---
+++ Support.UnitTests/ExtensionsTests/StringFormatExtensionTests.cs
@@ -0,0 +1,18 @@
+﻿using Support.Microservice.Extensions;
+
+namespace Support.UnitTests.ExtensionsTests;
+public sealed class StringFormatExtensionTests
+{
+    [Fact]
+    public void FormatTo_ReturnsFormatedString()
+    {
+        // A
+        var stringToFormat = "{0} meu nome é {1}";
+
+        // A
+        var formattedString = stringToFormat.FormatTo("oi", "joao");
+
+        // A
+        Assert.Equal("oi meu nome é joao", formattedString);
+    }
+}
