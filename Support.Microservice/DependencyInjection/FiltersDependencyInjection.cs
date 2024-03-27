c79ec28c7471b1ab603e36f2a584e20430daa902
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-26 21:35:04 -0300

    Feat: Adding Notification Pattern.

---
+++ Support.Microservice/DependencyInjection/FiltersDependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Support.Microservice.Filters;
+
+namespace Support.Microservice.DependencyInjection;
+
+internal static class FiltersDependencyInjection
+{
+    internal static void AddFilterDependencyInjection(this IServiceCollection services)
+    {
+        services.AddScoped<NotificationFilter>();
+        services.AddMvc(options => options.Filters.AddService<NotificationFilter>());
+    }
+}
