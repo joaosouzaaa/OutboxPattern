89f9a2b88be8b0edae5120abc4d1e12b70487bde
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-26 22:02:25 -0300

    Feat: Adding AppDbContext.

--- Support.Microservice/DependencyInjection/DependencyInjectionHandler.cs
+++ Support.Microservice/DependencyInjection/DependencyInjectionHandler.cs
@@ -1,10 +1,27 @@
-﻿namespace Support.Microservice.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Support.Microservice.Constants;
+using Support.Microservice.Data.DatabaseContexts;
+using Support.Microservice.Factories;
+
+namespace Support.Microservice.DependencyInjection;
 
 internal static class DependencyInjectionHandler
 {
     internal static void AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCorsDependencyInjection();
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            var connectionString = configuration.GetConnectionString();
+            options.UseSqlServer(connectionString);
+
+            if (Environment.GetEnvironmentVariable(EnvironmentVariables.Environment) is EnvironmentsConstants.Development)
+            {
+                options.EnableDetailedErrors();
+                options.EnableSensitiveDataLogging();
+            }
+        });
 
         services.AddSettingsDependencyInjection();
         services.AddFilterDependencyInjection();
