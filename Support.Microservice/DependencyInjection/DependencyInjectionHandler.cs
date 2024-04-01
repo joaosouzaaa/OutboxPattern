d90d360c96fc493ffd60bd70674e475741f4e528
joaosouzaaa <joaoasouza982@gmail.com>
2024-04-01 18:11:23 -0300

    Feat: Adding MappersDependencyInjection.

--- Support.Microservice/DependencyInjection/DependencyInjectionHandler.cs
+++ Support.Microservice/DependencyInjection/DependencyInjectionHandler.cs
@@ -27,7 +27,7 @@ internal static class DependencyInjectionHandler
         services.AddFilterDependencyInjection();
         services.AddRepositoriesDependencyInjection();
         services.AddOptionsDependencyInjection(configuration);
-
+        services.AddMappersDependencyInjection();
         services.AddValidatorsDependencyInjection();
     }
 }
